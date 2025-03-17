1695c4b5fe2eedf38fa67bbc6d01f4b09b4926a9
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2025-03-18 01:27:51 +0700

    learn : prisma

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,22 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProductsService {
-  private readonly product = [
-    {
-      id: 1,
-      product: 'Product 1',
-      description: 'item 1',
-    },
-    { id: 2, product: 'Product 2', description: 'item 2' },
-  ];
-
-  findAll() {
-    return this.product;
+  constructor(private readonly databaseService: DatabaseService) {}
+  async create(createProductDto: Prisma.ProductCreateInput) {
+    return this.databaseService.product.create({
+      data: createProductDto,
+    });
   }
 
-  create(product) {
-    this.product.push(product);
-    return this.product;
+  async findAll() {
+    return this.databaseService.product.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.product.findUnique({
+      where: { id },
+      include: {
+        tags: true,
+        description: true,
+        reviews: true,
+      },
+    });
+  }
+
+  async update(id: number, updateProductDto: Prisma.ProductUpdateInput) {
+    return this.databaseService.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+  }
+  remove(id: number) {
+    return this.databaseService.product.delete({
+      where: { id },
+    });
   }
 }
