6910722b2582e0fe78344a89d35f5cab8a74c0cd
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2025-03-18 18:15:07 +0700

    setup : database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { DatabaseController } from './database/database.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule],
+  controllers: [AppController, DatabaseController],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
