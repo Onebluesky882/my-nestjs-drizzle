0e21936906dec8f7465b8f8cc2215dc765bb7705
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2025-03-28 11:36:18 +0700

    install : nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ProductsModule } from './products/products.module';
-import { UsersModule } from './users/users.module';
-import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [DatabaseModule, ProductsModule, UsersModule, OrderModule],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
 import { DatabaseController } from './database.controller';
+import { DatabaseService } from './database.service';
 
 @Module({
-  exports: [DatabaseService],
   controllers: [DatabaseController],
-  providers: [DatabaseService],
+  providers: [DatabaseService]
 })
 export class DatabaseModule {}
